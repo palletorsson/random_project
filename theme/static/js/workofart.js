2b4adc90433b85e435ccbe46c32ed322466bcd4c
palle torsson <palle.torsson@gmail.com>
2013-12-11 10:01:36 +0100

    removing api call

--- theme/static/js/workofart.js
+++ theme/static/js/workofart.js
@@ -77,10 +77,10 @@ WORKOFART.Related = function (value) {
 }
 
 
+var postId = $('#post_id').text();
+var api = false;
 
-// var postId =  $('#post_id').text();
-
-if (postId) {
+if (api) {
 	$.get("http://127.0.0.1:8000/api/related/?format=json&post="+postId, function( data ) {
 
 		$.each(data.objects, function() {
